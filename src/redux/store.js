22140a5492101c1269701fc41077573e89124a3c
Htoomyat-Kyaing <94776906+Htoomyat-Kyaing@users.noreply.github.com>
2024-06-01 14:13:38 +0630

    added redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "../redux/user/userSlice";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { user: userReducer },
+  // does not need curly brackets
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleWare) =>
+    getDefaultMiddleWare({ serializableCheck: false }),
 });
+
+export const persistor = persistStore(store);
